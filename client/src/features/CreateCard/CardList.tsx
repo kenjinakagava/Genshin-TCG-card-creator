7f4eb610243aa30e794c506152a5f54cc68fd3e9
Kenji.N <1314webdev@gmail.com>
2023-02-07 14:57:56 -0300

    Add Edit Form

--- client/src/features/CreateCard/CardList.tsx
+++ client/src/features/CreateCard/CardList.tsx
@@ -4,7 +4,7 @@ interface CardListProps {
 
 const CardList = ({ children }: CardListProps) => {
   return (
-    <ul className="min-h-screen flex flex-col items-center gap-8 justify-evenly md:flex-row flex-wrap container mx-auto py-8">
+    <ul className="min-h-screen flex flex-col items-center gap-32 justify-evenly md:flex-row flex-wrap container mx-auto pt-8 pb-32 md:gap-8 ">
       {children}
     </ul>
   );
